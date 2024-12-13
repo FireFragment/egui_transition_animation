d9a9234a02aa3f7263bb7bd4f6ee79259cec8964
FireFragment <55660550+FireFragment@users.noreply.github.com>
2024-12-13 20:11:09 +0100

    Egui hello world

---
+++ flake.nix
@@ -0,0 +1,102 @@
+{
+  description = "Rust broker-v2 dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixkgs";
+    };
+    crate2nix.url = "github:nix-community/crate2nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    crate2nix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+
+        buildRustCrateForPkgs = pkgs: pkgs.buildRustCrate.override {
+          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+            rav1e = attrs: {
+              CARGO_ENCODED_RUSTFLAGS = "";
+            };
+          };
+        };
+
+        crate2nix-tools = pkgs.callPackage "${crate2nix}/tools.nix" {};
+
+        generatedCargoNix = crate2nix-tools.generatedCargoNix {
+            name = "egui_transition_example";
+            src = ./.;
+        };
+
+        cargoNix = pkgs.callPackage "${generatedCargoNix}/default.nix" {
+          inherit buildRustCrateForPkgs;
+        };
+
+        # TODO: Is anything superflous here?
+        eguiLibs = with pkgs; [
+          wayland
+          libxkbcommon
+          libGL
+          libGLU
+        ] ++ (with pkgs.xorg; [
+          libX11
+          libxcb
+          libXcursor
+          libXrandr
+          libXi
+          pkg-config
+        ]);
+      in {
+        packages.default = pkgs.symlinkJoin {
+          name = "egui_transition_example";
+          paths = [ cargoNix.rootCrate.build ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/egui_transition \
+              --suffix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath eguiLibs}
+              mv $out/bin/egui_transition $out/bin/egui_transition_example
+          '';
+        };
+        #;
+
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = [
+            (pkgs.rust-bin.stable.latest.default.override {
+                  extensions = [ "rust-src" "cargo" "rustc" "clippy" "rust-analyzer" ];
+            })
+            pkgs.gcc
+          ] ++ eguiLibs;
+
+          shellHook = ''
+              export LD_LIBRARY_PATH=/run/opengl-driver/lib/:${pkgs.lib.makeLibraryPath eguiLibs}
+          '';
+
+          RUST_SRC_PATH = "${pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+          }}/lib/rustlib/src/rust/library";
+
+
+          buildInputs = with pkgs; [
+            openssl.dev
+            glib.dev
+            pkg-config
+          ];
+        };
+      }
+    );
+}
