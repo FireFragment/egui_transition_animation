83d90e1bc31e5cfba5074d9f41a030048e93b792
FireFragment <55660550+FireFragment@users.noreply.github.com>
2025-04-20 12:51:34 +0200

    Create MVP `egui_animated_selectable_value` crate

---
+++ animated_selectable_value/examples/simple.rs
@@ -0,0 +1,44 @@
+fn main() -> eframe::Result {
+    use eframe::egui;
+    use egui_animated_selectable_value::prelude::*;
+
+    #[derive(PartialEq, PartialOrd, Clone, Eq, Hash)]
+    enum Page {
+        Page1,
+        Page2,
+        Page3,
+        Page4,
+    }
+
+    let mut page = Page::Page1;
+    let mut page_2 = Page::Page1;
+
+    eframe::run_simple_native(
+        "Egui page transition example",
+        Default::default(),
+        move |ctx, _frame| {
+            ctx.style_mut(|style| style.animation_time = 0.2);
+            egui::CentralPanel::default().show(ctx, |ui| {
+                ui.add_space(100.0);
+
+                ui.horizontal(|ui| {
+                    selectable_value(ui, 0, &mut page, Page::Page1, "Page 1");
+                    selectable_value(ui, 0, &mut page, Page::Page2, "Page 2 aaaaaaaaaa");
+                    selectable_value(ui, 0, &mut page, Page::Page3, "Page 3");
+                    selectable_value(ui, 0, &mut page, Page::Page4, "Page 4");
+                });
+
+                ui.add_space(32.0);
+
+                ui.horizontal(|ui| {
+                    selectable_value(ui, 1, &mut page_2, Page::Page1, "Page 1");
+                    selectable_value(ui, 1, &mut page_2, Page::Page2, "Page 2 aaaaaaaaaa");
+                });
+                ui.horizontal(|ui| {
+                    selectable_value(ui, 1, &mut page_2, Page::Page3, "Page 3");
+                    selectable_value(ui, 1, &mut page_2, Page::Page4, "Page 4");
+                });
+            });
+        },
+    )
+}
---
+++ animated_selectable_value/src/lib.rs
@@ -0,0 +1,106 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+use egui::{
+    emath::easing, epaint, Button, Color32, CornerRadius, Id, LayerId, Order, Pos2, Rect, Shape,
+    Stroke, UiBuilder, Vec2,
+};
+use egui_animation::animate_eased;
+
+pub mod prelude {
+    pub use super::selectable_value;
+}
+
+pub fn selectable_value<T: Eq>(
+    ui: &mut egui::Ui,
+    group_id: impl Hash,
+    current_value: &mut T,
+    selected_value: T,
+    text: &str,
+) -> egui::Response {
+    // A hash uniquely indentifiyng this widget
+    /*let widget_hash = {
+        let mut hasher = DefaultHasher::new();
+        group_id.hash(&mut hasher);
+        selected_value.hash(&mut hasher);
+        hasher.finish()
+    };*/
+
+    let selected = *current_value == selected_value;
+
+    let frame_shape_id = ui.painter().add(Shape::Noop);
+
+    let label_response = ui.add(
+        Button::new(text)
+            .selected(selected)
+            // Enable frame to have padding, but disable border and fill, because we draw our own frame
+            .frame(true)
+            .fill(Color32::TRANSPARENT)
+            .stroke(Stroke::NONE),
+    );
+    let rect = label_response.rect;
+
+    let visuals = ui.style().interact_selectable(&label_response, true);
+
+    if selected {
+        ui.painter().set(
+            frame_shape_id,
+            egui::Frame::new()
+                .fill(visuals.bg_fill)
+                .stroke(visuals.bg_stroke)
+                .corner_radius(visuals.corner_radius)
+                .paint(animate_rect_with_time(
+                    ui.ctx(),
+                    Id::new(group_id),
+                    rect,
+                    0.2,
+                )),
+        );
+    }
+
+    if label_response.clicked() {
+        *current_value = selected_value;
+    }
+
+    label_response
+}
+
+pub fn animate_rect_with_time(
+    ctx: &egui::Context,
+    id: Id,
+    target_value: Rect,
+    animation_time: f32,
+) -> Rect {
+    let min_x = animate_eased(
+        ctx,
+        id.with("animate_rect_with_time, min.x"),
+        target_value.min.x,
+        animation_time,
+        easing::circular_in_out,
+    );
+    let min_y = animate_eased(
+        ctx,
+        id.with("animate_rect_with_time, min.y"),
+        target_value.min.y,
+        animation_time,
+        easing::circular_in_out,
+    );
+    let max_x = animate_eased(
+        ctx,
+        id.with("animate_rect_with_time, max.x"),
+        target_value.max.x,
+        animation_time,
+        easing::circular_in_out,
+    );
+    let max_y = animate_eased(
+        ctx,
+        id.with("animate_rect_with_time, max.y"),
+        target_value.max.y,
+        animation_time,
+        easing::circular_in_out,
+    );
+
+    Rect {
+        min: Pos2 { x: min_x, y: min_y },
+        max: Pos2 { x: max_x, y: max_y },
+    }
+}
